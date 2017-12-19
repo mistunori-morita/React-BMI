0c9c6e5bbc81e90e16bf6644eab93bc79b424cc3
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2017-12-19 19:55:22 +0900

    core component make

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import Range from './components/Range';
+import Output from './components/Output';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      height: 170,
+      weight: 65,
+      bmi: 22.49,
+      bmiClass: 'Normal'
+    }
+  }
   render() {
     return (
       <div className="App">
-      
+        <h1>BMI Caluclator</h1>
+        <form>
+          <div>
+            <label>Hieght</label>
+            <Range />
+          </div>
+          <div>
+            <label>Weight</label>
+
+          </div>
+        </form>
+        <br/><br/>
+        <Output />
       </div>
     );
   }
--- src/components/Range.js
+++ src/components/Range.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class Range extends Component {
   render() {
     return (
       <div className="range">
-
+       <input type="range" />
       </div>
     );
   }
